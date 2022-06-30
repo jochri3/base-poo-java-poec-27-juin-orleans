c7d6014afeb44d93eacf1aef87f3383e551959dd
Christian Lisangola <jochri3@gmail.com>
2022-06-30 12:01:57 +0300

    Add Avion class without visibility modifiers

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@
 
 public class Main {
     public static void main(String[] args) {
+        Avion boeing737Max=new Avion();
+        boeing737Max.constructeur="Boeing";
+        boeing737Max.nombreSieges=200;
+        boeing737Max.longueur=70;
+
+        Avion airbus350f=new Avion();
+        airbus350f.constructeur="AIrbus";
+        airbus350f.nombreSieges=250;
+        airbus350f.longueur=60;
     }
 }
