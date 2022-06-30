afc4680bfafa585e089b8fcac525f84eba63dbe5
Christian Lisangola <jochri3@gmail.com>
2022-06-30 12:23:29 +0300

    Change implementation of getters/setters in main class

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Affichage Boeing 737 Max\n=====================");
         Avion boeing737Max=new Avion();
-        boeing737Max.constructeur="Boeing";
-        boeing737Max.nombreSieges=200;
-        boeing737Max.longueur=70;
+        boeing737Max.setConstructeur("Boeing");
+        boeing737Max.setNombreSieges(200);
+        boeing737Max.setLongueur(70);
         System.out.printf("Constructeur : %s\nSieges : %d\nLongueur : %f\n",
-                boeing737Max.constructeur,boeing737Max.nombreSieges,boeing737Max.longueur);
+                boeing737Max.getConstructeur(),boeing737Max.getNombreSieges(),
+                boeing737Max.getLongueur());
 
         System.out.println("Affichage Airbus 350F\n=====================");
         Avion airbus350f=new Avion();
-        airbus350f.constructeur="AIrbus";
-        airbus350f.nombreSieges=250;
-        airbus350f.longueur=60;
+        airbus350f.setConstructeur("Airbus");
+        airbus350f.setNombreSieges(250);
+        airbus350f.setLongueur(60);
         System.out.printf("Constructeur : %s\nSieges : %d\nLongueur : %f\n",
-                airbus350f.constructeur,airbus350f.nombreSieges,airbus350f.longueur);
+                airbus350f.getConstructeur(),airbus350f.getNombreSieges(),airbus350f.getLongueur());
     }
 }
