4c94eba26f4fb727ee64fa24ef014bace25e0502
Christian Lisangola <jochri3@gmail.com>
2022-06-30 12:13:17 +0300

    Add getter in Avion class

--- src/Avion.java
+++ src/Avion.java
@@ -1,5 +1,19 @@
 class Avion{
-   int nombreSieges;
-   String constructeur;
-   float longueur;
+   private int nombreSieges;
+   private String constructeur;
+   private float longueur;
+
+
+   public int getNombreSieges(){
+      return nombreSieges;
+   }
+
+   public String getConstructeur(){
+      return constructeur;
+   }
+
+   public float getLongueur(){
+      return longueur;
+   }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Affichage Boeing 737 Max\n=====================");
         Avion boeing737Max=new Avion();
         boeing737Max.constructeur="Boeing";
         boeing737Max.nombreSieges=200;
         boeing737Max.longueur=70;
+        System.out.printf("Constructeur : %s\nSieges : %d\nLongueur : %f\n",
+                boeing737Max.constructeur,boeing737Max.nombreSieges,boeing737Max.longueur);
 
+        System.out.println("Affichage Airbus 350F\n=====================");
         Avion airbus350f=new Avion();
         airbus350f.constructeur="AIrbus";
         airbus350f.nombreSieges=250;
         airbus350f.longueur=60;
+        System.out.printf("Constructeur : %s\nSieges : %d\nLongueur : %f\n",
+                airbus350f.constructeur,airbus350f.nombreSieges,airbus350f.longueur);
     }
 }
